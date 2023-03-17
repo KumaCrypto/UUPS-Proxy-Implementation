25e6c1fb5ee2eff31c9629252be6ef896b93b2a5
Vladimir Kumalagov <vkumalagov@icloud.com>
2023-03-17 20:13:16 +0100

    fix: add initializer to UUPSUpgradeableTest

--- contracts/Test/UUPSUpgradeable.t.sol
+++ contracts/Test/UUPSUpgradeable.t.sol
@@ -2,15 +2,20 @@
 pragma solidity 0.8.19;
 
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import {UUPSUpgradeable} from "../UUPSUpgradeable.sol";
 
-contract UUPSUpgradeableTest is UUPSUpgradeable, OwnableUpgradeable {
-	function getVersion() external pure returns (uint256) {
-		return 1;
+contract UUPSUpgradeableTest is
+	Initializable,
+	UUPSUpgradeable,
+	OwnableUpgradeable
+{
+	function initialize() external initializer {
+		__Ownable_init();
 	}
 
-	function initialize() external {
-		__Ownable_init();
+	function getVersion() external pure returns (uint256) {
+		return 1;
 	}
 
 	function _authorizeUpgrade(
